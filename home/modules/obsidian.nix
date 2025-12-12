694de3101cda32cffbaad506f74654b2b3443449
brandonboo2024 <brandonboo2024@computing.smu.edu.sg>
2025-12-13 01:22:06 +0800

    lazy to configure community plugins

--- home/modules/obsidian.nix
+++ home/modules/obsidian.nix
@@ -1,11 +1,33 @@
-{pkgs,...}:
-{
-  programs.obsidian = {
-    enable = true;
-    vaults.Brandon1 = {
-        target = "Brandon1";
-        settings.corePlugins = ["sync"];
-    };
-  };
-  stylix.targets.obsidian.vaultNames = [ "Brandon1" ];
-}
+# commented out as too lazy to configure community plugins as well, standard app works fine
+
+# {pkgs,...}:
+# {
+#   programs.obsidian = {
+#     enable = true;
+#     vaults.Brandon1 = {
+#         target = "Brandon1";
+#         settings.corePlugins = [
+#           "sync"
+#           "backlink"
+#           "bookmarks"
+#           "canvas"
+#           "command-palette"
+#           "daily-notes"
+#           "editor-status"
+#           "file-explorer"
+#           "file-recovery"
+#           "global-search"
+#           "graph"
+#           "note-composer"
+#           "outgoing-link"
+#           "outline"
+#           "page-preview"
+#           "switcher"
+#           "tag-pane"
+#           "templates"
+#           "word-count"
+#         ];
+#     };
+#   };
+#   stylix.targets.obsidian.vaultNames = [ "Brandon1" ];
+# }
