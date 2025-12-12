3c521923f4de2fc61262df7a0ba705e1ab881ace
brandonboo2024 <brandonboo2024@computing.smu.edu.sg>
2025-12-13 01:27:21 +0800

    a

--- home/base.nix
+++ home/base.nix
@@ -20,7 +20,7 @@ in
     ./modules/swayidle.nix
     ./modules/firefox.nix
     ./modules/bash.nix
-    ./modules/obsidian.nix
+    # ./modules/obsidian.nix
   ];
   programs.git = {
     enable = true;
