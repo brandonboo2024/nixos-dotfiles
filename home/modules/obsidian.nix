6f5ae663d3fcc09f28df9494556c3bcd09f44eac
brandonboo2024 <brandonboo2024@computing.smu.edu.sg>
2025-12-13 00:50:00 +0800

    stuff

--- home/modules/obsidian.nix
+++ home/modules/obsidian.nix
@@ -3,6 +3,9 @@
   programs.obsidian = {
     enable = true;
     vaults.Brandon1.target = "Brandon1";
+    vaults.Brandon1.settings.corePlugins = {
+        sync.enable = true;
+    };
   };
   stylix.targets.obsidian.vaultNames = [ "Brandon1" ];
 }
