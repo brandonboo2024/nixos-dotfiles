def1753324dd65ddd68a58da7e4457e7cc8653d1
brandonboo2024 <brandon.boo.2024@computing.smu.edu.sg>
2025-12-19 22:10:00 +0800

    huh

--- hosts/SystemModules/bootloader.nix
+++ hosts/SystemModules/bootloader.nix
@@ -3,8 +3,9 @@
     efi = {
       canTouchEfiVariables = true;
     };
-
+		refind.enable = false;
     systemd-boot.enable = true;
 		systemd-boot.consoleMode = "2";
+		systemd-boot.configurationLimit = 10;
   };
 }
